e4dd3003b973e06e64fb9aab791fe5da42ea0f0b
haruka <144975485+g22934@users.noreply.github.com>
2023-09-27 11:17:25 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -22,7 +22,7 @@ const myImage = document.querySelector("img");
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute("src", "images/firefox2.png");
+    myImage.setAttribute("src", "images/kitune.png");
   } else {
     myImage.setAttribute("src", "images/firefox-icon.png");
   }
